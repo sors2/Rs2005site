b74c247f7ea6c07ddce1db901e7e45aaca71bdd2
sors2 <sors13@live.com>
2021-02-17 12:48:35 +0100

    Added all the folder to Rs.com

--- rs.com/classic/manual/Manual-Main-04-13_files/07-25Quests_files/graph-calc.js
+++ rs.com/classic/manual/Manual-Main-04-13_files/07-25Quests_files/graph-calc.js
@@ -1,9 +1,9 @@
 function getEventX(event) {
 	var posx = 0;
 	if (event.pageX || event.pageY) {
-		posx =  event.pageX;
+		posx = event.pageX;
 	}
-	else if (event.clientX || event.clientY) 	{
+	else if (event.clientX || event.clientY) {
 		posx = event.clientX + document.body.scrollLeft
 			+ document.documentElement.scrollLeft;
 	}
@@ -18,11 +18,11 @@ function getElementX(obj) {
 	}
 	return x;
 }
-function zeroPad(str,len) {
+function zeroPad(str, len) {
 	var i;
 	var pad = "";
 	var s = str.toString();
-	for(i=s.length; i < len; i++) {
+	for (i = s.length; i < len; i++) {
 		pad = "0".toString() + pad.toString();
 	}
 	return pad.toString() + s.toString();
@@ -30,21 +30,21 @@ function zeroPad(str,len) {
 
 function dateToTimestamp(date) {
 	return date.getFullYear() +
-		zeroPad(date.getMonth()+1,2) +
-		zeroPad(date.getDay()+1,2) +
-		zeroPad(date.getHours(),2) +
-		zeroPad(date.getMinutes(),2) +
-		zeroPad(date.getSeconds(),2);
+		zeroPad(date.getMonth() + 1, 2) +
+		zeroPad(date.getDay() + 1, 2) +
+		zeroPad(date.getHours(), 2) +
+		zeroPad(date.getMinutes(), 2) +
+		zeroPad(date.getSeconds(), 2);
 }
 
-function calcTimestamp(event,element,firstMS,lastMS) {
-	  var eventX = getEventX(event);
-	  var elementX = getElementX(element);
-	  var elementWidth = element.width;
-	  var msWidth = lastMS - firstMS;
-	  var x = eventX - elementX;
-	  var pct = x / elementWidth;
-	  var pctDate = pct * msWidth;
-	  var date = pctDate + firstMS;
-	  return dateToTimestamp(new Date(date));
+function calcTimestamp(event, element, firstMS, lastMS) {
+	var eventX = getEventX(event);
+	var elementX = getElementX(element);
+	var elementWidth = element.width;
+	var msWidth = lastMS - firstMS;
+	var x = eventX - elementX;
+	var pct = x / elementWidth;
+	var pctDate = pct * msWidth;
+	var date = pctDate + firstMS;
+	return dateToTimestamp(new Date(date));
 }
