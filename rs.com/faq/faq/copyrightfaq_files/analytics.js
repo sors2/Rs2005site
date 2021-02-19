eb0ef4fabf80bd712601e6a78028bb357d531c4d
sors2 <sors13@live.com>
2021-02-19 01:40:30 +0100

    Image paths inside rs.com corrected

--- rs.com/faq/faq/copyrightfaq_files/analytics.js
+++ rs.com/faq/faq/copyrightfaq_files/analytics.js
@@ -2,25 +2,25 @@ var archive_analytics = {
     startTime: new Date(),
     img_src: "//analytics.archive.org/0.gif",
     values: { service: 'wb' },
+    
+    onload_func: function() {
+	var now = new Date();
+	var loadtime = now - archive_analytics.startTime;
 
-    onload_func: function () {
-        var now = new Date();
-        var loadtime = now - archive_analytics.startTime;
-
-        var v = archive_analytics.values;
-        v.loadtime = loadtime;
-        v.timediff = -(now.getTimezoneOffset() / 60);
-        v.locale = archive_analytics.get_locale();
-        // if no referrer set '-' as referrer
-        v.referrer = document.referrer || '-';
+	var v = archive_analytics.values;
+	v.loadtime = loadtime;
+	v.timediff = -(now.getTimezoneOffset()/60);
+	v.locale = archive_analytics.get_locale();
+	// if no referrer set '-' as referrer
+	v.referrer = document.referrer || '-';
 
         var string = archive_analytics.format_bug(v);
-        var loadtime_img = new Image(100, 25);
+        var loadtime_img = new Image(100,25);
         loadtime_img.src = archive_analytics.img_src + "?" + string;
     },
-    format_bug: function (values) {
+    format_bug: function(values) {
         var ret = ['version=2'], count = 2;
-
+        
         for (var data in values) {
             ret.push(encodeURIComponent(data) + "=" + encodeURIComponent(values[data]));
             count = count + 1;
@@ -28,17 +28,17 @@ var archive_analytics = {
         ret.push('count=' + count);
         return ret.join("&");
     },
-    get_locale: function () {
+    get_locale: function() {
         if (navigator) {
-            return navigator.language || navigator.browserLanguage ||
-                navigator.systemLanguage || navigator.userLanguage || '';
+	    return navigator.language || navigator.browserLanguage ||
+		navigator.systemLanguage || navigator.userLanguage || '';
         }
         return '';
     },
-    get_cookie: function (name) {
-        var parts = document.cookie.split(name + "=");
-        if (parts.length == 2) return parts.pop().split(";").shift();
-        return 0;
+    get_cookie: function(name) {
+      var parts = document.cookie.split(name + "=");
+      if (parts.length == 2) return parts.pop().split(";").shift();
+      return 0;
     }
 };
 
