79e8bc5b863690204dd723c778f61bbe003ff385
sors2 <sors13@live.com>
2021-02-19 19:45:49 +0100

    Removed Webclient --> Too many unnecessary files

--- webclient/RuneScape-MIDI-Player-Suite-master/src/main/AbstractSound.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import main.utils.Node;
-
-public abstract class AbstractSound extends Node {
-
-    public int position;
-
-}
--- webclient/classic/manual/Manual-Main-04-13_files/RulesOfConduct_files/main.js
+++
@@ -1,8 +0,0 @@
-try {
-	toppage = parent.location.href;
-} catch (er) {
-	top.location.href = "http://www.rscarchive.org/";
-}
-if (toppage == self.location.href || toppage.indexOf("www.rscarchive.org/") > 11 || toppage.indexOf("www.rscarchive.org/") == -1) {
-	top.location.href = "http://www.rscarchive.org/";
-}
