c121581f86be79a36ae9812356d8d4c7a29142ac
sors2 <sors13@live.com>
2021-02-28 15:25:56 +0100

    Update login.php

--- rs.com/login.php
+++ rs.com/login.php
@@ -35,7 +35,7 @@ if (isset($_POST["login"])){
                 $statement = $conn->prepare("UPDATE users SET last_activity = ? WHERE userID = ?");
                 $statement->bind_param("si",$dt,$user['userID']);
                 $statement->execute();
-                header('Location: login_frame.php');
+                header('Location: frame.php');
             }
             else{
                 echo "You are Banned.";
