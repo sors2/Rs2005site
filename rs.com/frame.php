14ea71aaa891152565ee3f19e57feb605a8ff7cc
sors2 <sors13@live.com>
2021-02-28 15:16:55 +0100

    ...

--- rs.com/frame.php
+++ rs.com/frame.php
@@ -1,3 +1,54 @@
+<?php 
+session_start();
+if (isset($_POST["login"])){
+    include "connect.php";
+
+    $username = $_POST['username'];
+    $pass = $_POST['pass'];
+    
+    $stmt= $conn->prepare("SELECT userID,user_password,rolesID FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();   
+    $result = $stmt->get_result();
+
+    if(mysqli_num_rows($result)== 0){
+        die("user not found");
+    }
+
+    $user = $result->fetch_assoc();
+
+        if(password_verify($pass,$user['user_password'])){
+
+            $stmt= $conn->prepare("SELECT expire FROM bandetails WHERE userID = ?");
+            $stmt->bind_param("i", $user['userID']);
+            $stmt->execute();   
+            $ban = $stmt->get_result();
+            $user_ban = $ban->fetch_assoc();
+        
+            $current = date("d-m-Y H:i:s");
+            if((mysqli_num_rows($ban) == 0) || strtotime($user_ban['duration']) < strtotime($current)){
+                session_start();
+                $_SESSION['username'] =$username;
+                $_SESSION['permission'] = $user['rolesID'];
+                $_SESSION['vote'] = "vote";
+                $dt = date('Y-m-d H:i:s');
+                $statement = $conn->prepare("UPDATE users SET last_activity = ? WHERE userID = ?");
+                $statement->bind_param("si",$dt,$user['userID']);
+                $statement->execute();
+                header('Location: login_frame.html');
+            }
+            else{
+                echo "You are Banned.";
+            }
+        }  
+        else{
+            echo "failed";
+        }
+    
+    mysqli_close($conn);
+}
+?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Frameset//EN">
 <html>
    <head>
