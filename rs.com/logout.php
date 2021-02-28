9447eba3d64bbd8c8c5e1e318080ea47457bda46
sors2 <sors13@live.com>
2021-02-28 13:50:37 +0100

    update for varrock, and loginframe

--- rs.com/logout.php
+++ rs.com/logout.php
@@ -1,5 +1,5 @@
 <?php
     session_start();
     unset($_SESSION['username']);
-    header('Location: title.php');
+    header('Location: runescape.html');
 ?>
--- rs.com/messagecentre/deletemessage.php
+++ rs.com/messagecentre/deletemessage.php
@@ -1,4 +1,6 @@
 <?php 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
     include "../connect.php";
     $n = 1;
     $stmt = $conn->prepare("UPDATE messagecentre SET `remove`= ? WHERE messageID=?");
