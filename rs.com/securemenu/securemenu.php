99c2715bc1a3e91a63c6c991700c8cc42161e418
sors2 <sors13@live.com>
2021-02-28 14:18:34 +0100

    Update securemenu.php

--- rs.com/securemenu/securemenu.php
+++ rs.com/securemenu/securemenu.php
@@ -51,8 +51,8 @@ function breakout_of_frame()
   // see https://www.thesitewizard.com/archive/framebreak.shtml
   // for an explanation of this script and how to use it on your
   // own website
-  if (top.location != location) {
-    top.location.href = document.location.href ;
+  if (top.location != frame.php) {
+    top.location.href = ../frame.php ;
   }
 }
 -->
