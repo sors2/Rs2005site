80f177667c78a53ae5244798b66ebe3706a18e3f
sors2 <sors13@live.com>
2021-03-07 09:12:54 +0100

    Update viewmessage.php

--- rs.com/messagecentre/viewmessage.php
+++ rs.com/messagecentre/viewmessage.php
@@ -64,9 +64,9 @@
                <table cellpadding=0 cellspacing=0>
                   <tr>
                      <td valign=top>
-					 <img src="../../img/edge_a.jpg" width=400 height=42 hspace=0 vspace=0></td>
+					 <img src="../../img/edge_a.jpg" width=100 height=82 hspace=0 vspace=0></td>
                      <td valign=top>
-					 <img src="../../img/edge_c.jpg" width=100 height=82 hspace=0 vspace=0></td>
+					 <img src="../../img/edge_c.jpg" width=400 height=42 hspace=0 vspace=0></td>
                      <td valign=top>
 					 <img src="../../img/edge_d.jpg" width=100 height=82 hspace=0 vspace=0></td>
                   </tr>
